13ba41bdb08713d68f346ae591725788a93ada4e
chiraag-kakar <chirucos1998@gmail.com>
2022-01-21 13:08:08 +0530

    added routes, models

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,10 @@ const dotenv = require("dotenv");
 const helmet = require("helmet");
 const morgan = require("morgan");
 
+const userRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
+const postRoute = require("./routes/posts");
+
 dotenv.config();
 
 mongoose.connect(
@@ -20,9 +24,10 @@ app.use(express.json());
 app.use(helmet());
 app.use(morgan("common"));
 
-app.get("/",(req,res)=>{
-    res.send("welcome my boy")
-})
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/posts", postRoute);
+
 app.listen(8080,()=>{
     console.log('Server Started')
 })
